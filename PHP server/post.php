1884134a5cd49cf5c2f34a4902a0f01a5484ff6a
kripanandjha <jha.kripanand@gmail.com>
2017-07-30 07:44:56 +0000

    adding post rides support

---
+++ PHP server/post.php
@@ -0,0 +1,22 @@
+<?php
+
+$data1 = file_get_contents('php://input');
+$data = json_decode($data1, true);
+$Source = $data["Source"];
+$Destination = $data["Destination"];
+$Date = $data["Date"];
+$Time = $data["Time"];
+$Username = $data['Username'];
+
+
+
+#$connect = mysqli_connect("localhost", "root", "#B@rahmanand1", "spartans");
+$connect = mysqli_connect("localhost", "spartanrides", "Spartan@SJSU2017", "spartans");
+$statement = mysqli_prepare($connect, "INSERT INTO post (username, source, destination, date, time) VALUES (?, ?, ?, ?, ?)");
+mysqli_stmt_bind_param($statement, "sssss", $Username, $Source, $Destination, $Date, $Time);
+mysqli_stmt_execute($statement);
+mysqli_stmt_close($statement);     
+$response = array();
+$response["success"] = true;
+echo json_encode($response);
+?>
