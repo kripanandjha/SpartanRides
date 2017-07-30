2f11c1add7170b87a689a1b46411621ba84f55b8
kripanandjha <jha.kripanand@gmail.com>
2017-07-30 07:48:33 +0000

    change databse user

--- PHP server/post.php
+++ PHP server/post.php
@@ -10,8 +10,7 @@ $Username = $data['Username'];
 
 
 
-#$connect = mysqli_connect("localhost", "root", "#B@rahmanand1", "spartans");
-$connect = mysqli_connect("localhost", "spartanrides", "Spartan@SJSU2017", "spartans");
+$connect = mysqli_connect("localhost", "user", "password", "database");
 $statement = mysqli_prepare($connect, "INSERT INTO post (username, source, destination, date, time) VALUES (?, ?, ?, ?, ?)");
 mysqli_stmt_bind_param($statement, "sssss", $Username, $Source, $Destination, $Date, $Time);
 mysqli_stmt_execute($statement);
