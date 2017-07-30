a9f377ed21975413abb4516eb91adfe063b0a272
Raunak Tiwari <raunakt91@gmail.com>
2017-07-29 18:16:51 -0700

    Updated UI for the app

---
+++ app/src/main/java/com/android/spartanrides/LoginActivity.java
@@ -0,0 +1,386 @@
+package com.android.spartanrides;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.firebase.client.Firebase;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Demonstrate Firebase Authentication using a Facebook access token.
+ */
+public class LoginActivity extends AppCompatActivity implements
+        View.OnClickListener {
+
+    private ViewPager viewPager;
+    private LinearLayout dotsLayout;
+    private int[] layouts;
+    private TextView btnBack, btnNext;
+    private ProgressDialog loading;
+
+    private static final String TAG = "FacebookLogin";
+
+    private TextView mStatusTextView;
+    private TextView mDetailTextView;
+
+    //Chat details
+    String firebaseUser;
+
+    // [START declare_auth]
+    private FirebaseAuth mAuth;
+    // [END declare_auth]
+
+    private CallbackManager mCallbackManager;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        FirebaseApp.initializeApp(this);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+        setContentView(R.layout.activity_login);
+
+        // Views
+//        mStatusTextView = (TextView) findViewById(R.id.status);
+//        mDetailTextView = (TextView) findViewById(R.id.detail);
+//        findViewById(R.id.button_facebook_signout).setOnClickListener(this);
+
+        // [START initialize_auth]
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        // [END initialize_auth]
+
+        // [START initialize_fblogin]
+        // Initialize Facebook Login button
+        mCallbackManager = CallbackManager.Factory.create();
+        final LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email", "public_profile");
+        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                loading = ProgressDialog.show(LoginActivity.this, null, "Signing In...", true, true);
+                loading.setCancelable(false);
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "facebook:onCancel");
+                // [START_EXCLUDE]
+                updateUI(null);
+                // [END_EXCLUDE]
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "facebook:onError", error);
+                // [START_EXCLUDE]
+                updateUI(null);
+                // [END_EXCLUDE]
+            }
+        });
+        // [END initialize_fblogin]
+
+        viewPager = (ViewPager) findViewById(R.id.view_pager);
+        dotsLayout = (LinearLayout) findViewById(R.id.layoutDots);
+        btnBack = (TextView) findViewById(R.id.btn_back);
+        btnNext = (TextView) findViewById(R.id.btn_next);
+
+        layouts = new int[]{
+                R.layout.info_slide1,
+                R.layout.info_slide2,
+                R.layout.info_slide3};
+
+        addBottomDots(0);
+
+        MyViewPagerAdapter myViewPagerAdapter = new MyViewPagerAdapter();
+        viewPager.setAdapter(myViewPagerAdapter);
+        viewPager.addOnPageChangeListener(viewPagerPageChangeListener);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int current = getItem(-1);
+                viewPager.setCurrentItem(current);
+            }
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int current = getItem(+1);
+                viewPager.setCurrentItem(current);
+            }
+        });
+    }
+
+    private void addBottomDots(int currentPage) {
+        TextView[] dots = new TextView[layouts.length];
+
+        int[] colorsActive = getResources().getIntArray(R.array.array_dot_active);
+        int[] colorsInactive = getResources().getIntArray(R.array.array_dot_inactive);
+
+        dotsLayout.removeAllViews();
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226;"));
+            dots[i].setTextSize(35);
+            dots[i].setTextColor(colorsInactive[currentPage]);
+            dotsLayout.addView(dots[i]);
+        }
+
+        if (dots.length > 0)
+            dots[currentPage].setTextColor(colorsActive[currentPage]);
+    }
+
+    private int getItem(int i) {
+        return viewPager.getCurrentItem() + i;
+    }
+
+    public class MyViewPagerAdapter extends PagerAdapter {
+        private LayoutInflater layoutInflater;
+
+        public MyViewPagerAdapter() {
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View view = layoutInflater.inflate(layouts[position], container, false);
+            container.addView(view);
+
+            return view;
+        }
+
+        @Override
+        public int getCount() {
+            return layouts.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object obj) {
+            return view == obj;
+        }
+
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            View view = (View) object;
+            container.removeView(view);
+        }
+    }
+
+    ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
+
+        @Override
+        public void onPageSelected(int position) {
+            addBottomDots(position);
+
+            if (position == layouts.length - 1) {
+                btnNext.setVisibility(View.GONE);
+                btnBack.setVisibility(View.VISIBLE);
+            } else if (position == 0) {
+                btnNext.setVisibility(View.VISIBLE);
+                btnBack.setVisibility(View.GONE);
+            } else {
+                btnNext.setVisibility(View.VISIBLE);
+                btnBack.setVisibility(View.VISIBLE);
+            }
+
+        }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
+
+        }
+    };
+
+    // [START on_start_check_user]
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+    // [END on_start_check_user]
+
+    // [START on_activity_result]
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Pass the activity result back to the Facebook SDK
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+    // [END on_activity_result]
+
+    // [START auth_with_facebook]
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+        // [START_EXCLUDE silent]
+//        showProgressDialog();
+        // [END_EXCLUDE]
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            if (loading.isShowing())
+                                loading.dismiss();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            if (loading.isShowing())
+                                loading.dismiss();
+                            updateUI(null);
+                        }
+
+                        // [START_EXCLUDE]
+//                        hideProgressDialog();
+                        // [END_EXCLUDE]
+                    }
+                });
+    }
+    // [END auth_with_facebook]
+
+    private void updateUI(FirebaseUser user) {
+//        hideProgressDialog();
+        if (user != null) {
+//            mStatusTextView.setText(getString(R.string.facebook_status_fmt, user.getDisplayName()));
+//            mDetailTextView.setText(getString(R.string.firebase_status_fmt, user.getUid()));
+            Log.d("LOGIN MSG", "Logged in Successfully");
+            findViewById(R.id.login_button).setVisibility(View.GONE);
+            updateUserInfo(user);
+            addUser();
+            Intent intent = new Intent(LoginActivity.this, Main2Activity.class);
+            startActivity(intent);
+            finish();
+//            findViewById(R.id.button_facebook_signout).setVisibility(View.VISIBLE);
+        } else {
+//            mStatusTextView.setText(R.string.signed_out);
+//            mDetailTextView.setText(null);
+            Log.d("LOGIN MSG", "Login Failed");
+//            findViewById(R.id.button_facebook_signout).setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+//        if (i == R.id.button_facebook_signout) {
+//            signOut();
+//        }
+    }
+
+    private void addUser() {
+        {
+//            final ProgressDialog pd = new ProgressDialog(getApplicationContext());
+//            pd.setMessage("Loading...");
+//            pd.show();
+            Firebase.setAndroidContext(getApplicationContext());
+
+            String url = "https://spartanride-173019.firebaseio.com/users.json";
+
+            StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String s) {
+                    Firebase reference = new Firebase("https://spartanride-173019.firebaseio.com/users");
+
+                    if (s.equals("null")) {
+                        reference.child(firebaseUser).child("password").setValue("qwerty");
+                        UserDetails.username = firebaseUser;
+//                        Toast.makeText(getApplicationContext(), "registration successful", Toast.LENGTH_LONG).show();
+                    } else {
+                        try {
+                            JSONObject obj = new JSONObject(s);
+                            if (!obj.has(firebaseUser)) {
+                                reference.child(firebaseUser).child("password").setValue("qwerty");
+                                UserDetails.username = firebaseUser;
+//                                Toast.makeText(getApplicationContext(), "registration successful", Toast.LENGTH_LONG).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+//                    pd.dismiss();
+                }
+
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError volleyError) {
+                    System.out.println("" + volleyError);
+//                    pd.dismiss();
+                }
+            });
+
+            RequestQueue rQueue = Volley.newRequestQueue(getApplicationContext());
+            rQueue.add(request);
+        }
+    }
+
+    public void updateUserInfo(FirebaseUser user) {
+        firebaseUser = user.getDisplayName();
+        UserDetails.username = firebaseUser;
+        UserDetails.fbUserID = user.getUid();
+        UserDetails.emailID = user.getEmail();
+        UserDetails.photoURL = user.getPhotoUrl().toString();
+        UserDetails.accessToken = AccessToken.getCurrentAccessToken().getToken();
+    }
+}
