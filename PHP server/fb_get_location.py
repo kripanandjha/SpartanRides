72e3b2efff3fede54fab742dcfb10be1ab33c108
kripanandjha <jha.kripanand@gmail.com>
2017-08-08 19:27:23 +0000

    adding multi threading support and users data comparison

--- PHP server/fb_get_location.py
+++ PHP server/fb_get_location.py
@@ -3,25 +3,31 @@
 import os
 import json
 import facebook
-import numpy
+import numpy as np
+import MySQLdb
+import re
+import threading
 
-token = 'EAACEdEose0cBAF2KhigSemoD2OMXheGP1pPratNa03D0eqvnMB1cRJ3eU0yyjx47ONcayU8zVqZBHmOOtnKpozxuA1PtK3h6MPyqt3ouD6tAK16CZCqN7n0X2UlHL9ht3NAUPddvc2397ATfqeENjrlviJIgNGDHoDSSg4zkBLpexSwS8yD8m690BlZAS8ZD'
+
+token = 'EAACEdEose0cBAJqIx8cNhECBEad4TmhdlY4ZBiQ77ZBApA0FgpA6CcJKIaqonK1foGfdHUF8Bj9SFn1YKXU2cBIZBFR9lkgiGWELgcv6lZCnyZAPnlLUcqnPzYiYTUi2XjYFhJbOiBjdrQbs3Pu5vhaJsJnWMIXaZBrocDKQc9QkottU1J7Cj1ZCicaXgbdae0ZD'
+
 graph = facebook.GraphAPI(token)
 
-profile = graph.get_object('me', fields = 'name, location, likes, friends')
-
-#print(json.dumps(profile, indent = 4))
-
+profile = graph.get_object('me', fields = 'name, location')
 location = profile['location']
 location = location['id']
+# create location folder it is does not exist
+if not os.path.exists(location):
+    profile = graph.get_object('me', fields = 'name, location, likes, friends')
+    os.makedirs(location)
+
+
+#print(json.dumps(profile, indent = 4))
 
 profile_id = profile['id']
 user_file = location + '/' + profile_id
 #print('profile id ', profile_id)
 
-# create location folder it is does not exist
-if not os.path.exists(location):
-    os.makedirs(location)
 
 # create userid file if it is does not exist
 if not os.path.exists(user_file):
@@ -40,3 +46,70 @@ if not os.path.exists(user_file):
 		myfile.write(likes[row]['id']+'\n')
 
 	myfile.close()
+
+
+# now load data from the user_file into numpy array
+user_data = np.loadtxt(user_file, dtype=str)
+
+
+
+#execute mysql query to fetch fb user id after the current date
+#select FBID  from post1 where date <= curdate();
+conn = MySQLdb.connect("localhost", "root", "#B@rahmanand1", "spartans")
+c = conn.cursor()
+c.execute("select FBID  from post1 where date <= curdate()")
+row = c.fetchall()
+st = str(row)
+st = st.replace("(", " ")
+st = st.replace(")", " ")
+st = st.replace(",", " ")
+st = st.replace(",", " ")
+st = st.replace("'", " ")
+l1 = st.split()
+np_post =  np.array(l1)
+#st = re.sub("[^\d\.]", " ", st)
+#for eacrhrow in row:
+#print type(user_data)
+#print type(np_post)
+set_a = set(np_post) # post set
+set_b = set(user_data) # logged in user info
+#print (set(user_data).intersection(np_post))
+final_data = set(set_a).intersection(set_b)
+not_common = final_data.symmetric_difference(set_a)
+final = np.array(final_data)
+not_common = list(not_common)
+not_common = np.array(not_common)
+not_common = np.delete(not_common, 0)
+print(not_common)
+#now extracting the data from the posted user data and store into numpy array
+np_post_data = None
+user_len = len(not_common)
+set_a = set_b
+final_post = np.zeros(user_len)
+t = []
+for i in range(0, user_len):
+	if os.path.exists(location+'/'+not_common[i]):
+		np_post_data[i] = np.loadtxt(location+'/'+not_common[i], dtype=str)
+		thread = threading.Thread(target=compare, args=(i,))
+		t.append(thread)
+#for i in range(0, user_len):
+#	print(i)
+#	t[i].start()
+
+for i in t:
+	i.start()
+for i in t:
+	i.join()
+# These below code should be run in multithreading environment
+#print(np_post
+def compare(num):
+	set_a = set(np_post_data[num])
+	final_data = set(set_a).intersection(set_b)
+	if not bool(final_data):
+		#add this user id to and array
+		final_post[num] = not_common[i]
+
+#for i in range(0, user_len):
+#	t[i] = threading.Thread(target=compare, args=(i,))
+print('after returning from all threads')
+print(final_post)
